b19c0f1d609df72c90d40374ee1c0b5ba71d41aa
T-Amary <taufik.amary@gmail.com>
2025-03-27 23:43:38 +0700

    feat: add current loc

--- lib/feature/add/add_provider.dart
+++ lib/feature/add/add_provider.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:amary_story/data/api/repository/story_repository.dart';
 import 'package:amary_story/feature/add/add_state.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geocoding/geocoding.dart' as geo;
 
 class AddProvider extends ChangeNotifier {
   final StoryRepository _repository;
@@ -19,6 +21,12 @@ class AddProvider extends ChangeNotifier {
 
   AddState _state = AddNoneState();
   AddState get state => _state;
+
+  Set<Marker> markers = {};
+  String address = "";
+  String street = "";
+  double latitude = -6.175372; // Default to Jakarta
+  double longitude = 106.827194; // Default to Jakarta
 
   set description(String value) {
     _description = value;
@@ -55,6 +63,31 @@ class AddProvider extends ChangeNotifier {
       _state = AddErrorState(message: e.toString());
       notifyListeners();
     }
+  }
+
+  Future<void> addMarker(double lat, double lon) async {
+    try {
+      final placemarks = await geo.placemarkFromCoordinates(lat, lon);
+      final place = placemarks.isNotEmpty ? placemarks.first : null;
+      final street = place?.street ?? "Tidak diketahui";
+      final address =
+          '${place?.subLocality}, ${place?.locality}, ${place?.postalCode}, ${place?.country}';
+      this.address = address;
+      this.street = street;
+    } catch (e) {
+      debugPrint("Error fetching address: $e");
+    }
+    latitude = lat;
+    longitude = lon;
+    markers.clear();
+    markers.add(
+      Marker(
+        markerId: const MarkerId('storyLocation'),
+        position: LatLng(lat, lon),
+        infoWindow: InfoWindow(title: street, snippet: address),
+      )
+    );
+    notifyListeners();
   }
 
   void resetToast() {
--- lib/feature/add/add_screen.dart
+++ lib/feature/add/add_screen.dart
@@ -6,7 +6,9 @@ import 'package:amary_story/widget/button/button_widget.dart';
 import 'package:amary_story/widget/toast/toast_enum.dart';
 import 'package:amary_story/widget/toast/toast_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:location/location.dart';
 import 'package:provider/provider.dart';
 
 class AddScreen extends StatefulWidget {
@@ -21,6 +23,7 @@ class AddScreen extends StatefulWidget {
 
 class _AddScreenState extends State<AddScreen> {
   XFile? _image;
+  GoogleMapController? _mapController;
 
   @override
   Widget build(BuildContext context) {
@@ -50,57 +53,152 @@ class _AddScreenState extends State<AddScreen> {
             provider.resetToast();
           });
 
-          return Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                Container(
-                  width: double.infinity,
-                  height: 200,
-                  decoration: BoxDecoration(
-                    color: Colors.grey[300],
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child:
-                      _image == null
-                          ? Icon(
-                            Icons.image,
-                            size: 100,
-                            color: Colors.grey[600],
-                          )
-                          : Image.file(File(_image!.path), fit: BoxFit.cover),
-                ),
-                SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () => _pickImage(ImageSource.gallery),
-                      child: Text("Add Image from Gallery"),
+          return SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                  ],
-                ),
-                SizedBox(height: 10),
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: "Description",
-                    border: OutlineInputBorder(),
+                    child:
+                        _image == null
+                            ? Icon(
+                              Icons.image,
+                              size: 100,
+                              color: Colors.grey[600],
+                            )
+                            : Image.file(File(_image!.path), fit: BoxFit.cover),
                   ),
-                  maxLines: 4,
-                  onChanged: (value) {
-                    provider.description = value;
-                  },
-                ),
-                SizedBox(height: 10),
-                ButtonWidget(
-                  isLoading: provider.state is AddLoadingState,
-                  isEnable: provider.isEnableButton,
-                  textButton: "Upload",
-                  onTap: () async {
-                    await provider.upload();
-                  },
-                ),
-              ],
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () => _pickImage(ImageSource.gallery),
+                        child: Text("Add Image from Gallery"),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "Description",
+                      border: OutlineInputBorder(),
+                    ),
+                    maxLines: 4,
+                    onChanged: (value) {
+                      provider.description = value;
+                    },
+                  ),
+                  SizedBox(height: 10),
+                  Card(
+                    elevation: 4,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(16),
+                      child: SizedBox(
+                        height: 350,
+                        child: Stack(
+                          children: [
+                            GoogleMap(
+                              initialCameraPosition: CameraPosition(
+                                target: LatLng(
+                                  provider.latitude,
+                                  provider.longitude,
+                                ),
+                                zoom: 18,
+                              ),
+                              onMapCreated: (controller) async {
+                                setState(() {
+                                  _mapController = controller;
+                                });
+
+                                await provider.addMarker(
+                                  provider.latitude,
+                                  provider.longitude,
+                                );
+                              },
+                              markers: provider.markers,
+                              myLocationButtonEnabled: false,
+                              zoomControlsEnabled: false,
+                              mapToolbarEnabled: false,
+                              myLocationEnabled: true,
+                              onLongPress: (LatLng latLng) {
+                                _onLongPressGoogleMap(latLng);
+                              },
+                            ),
+                            Positioned(
+                              top: 16,
+                              left: 16,
+                              right: 16,
+                              child: Container(
+                                padding: const EdgeInsets.all(12),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(10),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.black26,
+                                      blurRadius: 4,
+                                      spreadRadius: 2,
+                                    ),
+                                  ],
+                                ),
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      provider.street,
+                                      style: const TextStyle(
+                                        fontSize: 12,
+                                        color: Colors.black,
+                                      ),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                    Text(
+                                      provider.address,
+                                      style: const TextStyle(
+                                        fontSize: 10,
+                                        color: Colors.black,
+                                      ),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              bottom: 16,
+                              right: 16,
+                              child: FloatingActionButton(
+                                child: const Icon(Icons.my_location),
+                                onPressed: () {
+                                  _onMyLocationButtonPress();
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  ButtonWidget(
+                    isLoading: provider.state is AddLoadingState,
+                    isEnable: provider.isEnableButton,
+                    textButton: "Upload",
+                    onTap: () async {
+                      await provider.upload();
+                    },
+                  ),
+                ],
+              ),
             ),
           );
         },
@@ -117,5 +215,51 @@ class _AddScreenState extends State<AddScreen> {
         provider.image = File(pickedFile.path);
       });
     }
+  }
+
+  void _onMyLocationButtonPress() async {
+    final AddProvider provider = context.read<AddProvider>();
+    final Location location = Location();
+    late bool serviceEnabled;
+    late PermissionStatus permissionGranted;
+    late LocationData locationData;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        debugPrint("Location services is not available");
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        debugPrint("Location permission is denied");
+        return;
+      }
+    }
+
+    locationData = await location.getLocation();
+    final latLng = LatLng(locationData.latitude!, locationData.longitude!);
+
+    provider.addMarker(locationData.latitude!, locationData.longitude!);
+
+    _mapController?.animateCamera(CameraUpdate.newLatLng(latLng));
+  }
+
+  void _onLongPressGoogleMap(LatLng latLng) async {
+    final AddProvider provider = context.read<AddProvider>();
+    provider.addMarker(latLng.latitude, latLng.longitude);
+
+    _mapController?.animateCamera(CameraUpdate.newLatLng(latLng));
+  }
+
+  @override
+  void dispose() {
+    _mapController?.dispose();
+    super.dispose();
   }
 }
